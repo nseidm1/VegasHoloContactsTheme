b8b5ca16bd4b576329f60dfc22cc9352015414cf
nseidm1 <noah@noahseidman.com>
2014-01-26 04:48:04 -0500

    Vegas Holo Contacts Theme.

---
+++ VegasSMSHoloContacts/src/com/project/vegasholocontacts/VegasSMSSHoloContacts.java
@@ -0,0 +1,23 @@
+package com.project.vegasholocontacts;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+public class VegasSMSSHoloContacts extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		overridePendingTransition(0, 0);
+		try {
+			Intent intent = new Intent("com.project.vegassms.VEGAS_CONTACTS_THEME_PICKER");
+			startActivity(intent);
+		} catch(ActivityNotFoundException e) {
+			startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=com.project.vegassms")));
+		}
+		finish();
+		overridePendingTransition(0, 0);
+	}
+}
